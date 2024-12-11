6bc73e79bcb78b828f14751acd660f0e7b676846
Joyce Zheng <yz4867@nyu.edu>
2024-12-11 15:55:37 -0500

    delete redundant files

--- src/App.jsx
+++ src/App.jsx
@@ -17,47 +17,7 @@ import AnimatedFlower from "./components/AnimatedFlower";
 import Fireflies from "./components/Fireflies";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const worker = useRef(null);
-  const [ready, setReady] = useState(null);
-  const [disabled, setDisabled] = useState(false);
-  const [progressItems, setProgressItems] = useState([]);
 
-  // Inputs and outputs
-  const [input, setInput] = useState('I love walking my dog.');
-  const [sourceLanguage, setSourceLanguage] = useState('eng_Latn');
-  const [targetLanguage, setTargetLanguage] = useState('fra_Latn');
-  const [output, setOutput] = useState('');
-
-  const translate = () => {
-    setDisabled(true);
-    worker.current.postMessage({
-      text: input,
-      src_lang: sourceLanguage,
-      tgt_lang: targetLanguage,
-    });
-  }
-
-  useEffect(() => {
-    if (!worker.current) {
-      // Create the worker if it does not yet exist.
-      worker.current = new Worker(new URL("./worker.js", import.meta.url), {
-        type: "module",
-      });
-    }
-
-    // Create a callback function for messages from the worker thread.
-    const onMessageReceived = (e) => {
-      // TODO: Will fill in later
-    };
-
-    // Attach the callback function as an event listener.
-    worker.current.addEventListener("message", onMessageReceived);
-
-    // Define a cleanup function for when the component is unmounted.
-    return () =>
-      worker.current.removeEventListener("message", onMessageReceived);
-  });
 
   return (
     <>
